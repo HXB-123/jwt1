0d27b3015f53928e3d2a8f5e28962e8dd5311d97
hxb <1091512781@qq.com>
2023-05-19 12:56:43 +0800

    TEST

---
+++ src/test/java/com/atguigu/jwt/JHSDB_TestCase.java
@@ -0,0 +1,21 @@
+package com.atguigu.jwt;
+
+
+public class JHSDB_TestCase {
+
+    static class Test{
+       static ObjectHolder staticObj = new ObjectHolder();
+        ObjectHolder instanceObj = new ObjectHolder();
+
+        void foo(){
+            ObjectHolder localObj = new ObjectHolder();
+            System.out.println("done");
+        }
+    }
+    private static class ObjectHolder{}
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.foo();
+    }
+}
